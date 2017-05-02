48f4260b10f2a3f359508a8846113cc13d51941f
Marcelo <marcelo.dsantos@b2wdigital.com>
2017-05-02 10:58:27 -0300

    Criado teste para saber o email e status code que mais tem ocorrencia no log

--- src/test/java/br/com/moip/test/LogServiceTest.java
+++ src/test/java/br/com/moip/test/LogServiceTest.java
@@ -1,8 +1,11 @@
 package br.com.moip.test;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,10 +19,34 @@ public class LogServiceTest {
 	
 	@Autowired
 	private LogService service;
+	
+	private List<String> convertFileLogToString;
+	
+	@Before
+	public void init() throws Exception {
+		convertFileLogToString = this.service.convertFileLogToString();
+	}
 
 	@Test
-	public void verifica_tamanho_arquivo() throws Exception {
-		List<String> convertFileLogToString = this.service.convertFileLogToString();
-		Assert.assertEquals(convertFileLogToString.size(), 547);
+	public void verifica_tamanho_linhas_arquivo() throws Exception {
+		Assert.assertEquals(this.convertFileLogToString.size(), 547);
+	}
+	
+	@Test
+	public void verifica_maior_ocorrencia_email() throws Exception{
+		String emailExpected = "https://surrealostrich.com.br";
+		Map<String, Long> emails = this.service.getEmails(this.convertFileLogToString);
+		List<String> listEmail = emails.keySet().stream().limit(1).collect(Collectors.toList()); 
+		String email = listEmail.get(0);
+		Assert.assertEquals(email, emailExpected);
+	}
+	
+	@Test
+	public void verifica_maior_ocorrencia_status_code() throws Exception{
+		Long statusCodeExpected = 500l;
+		Map<Long, Long> statusCodes = this.service.getStatusCodes(this.convertFileLogToString);
+		List<Long> listStatusCodes = statusCodes.keySet().stream().limit(1).collect(Collectors.toList());
+		Long statusCode = listStatusCodes.get(0);
+		Assert.assertEquals(statusCode, statusCodeExpected);
 	}
 }
